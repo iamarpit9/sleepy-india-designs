27f6d37b90cfaaa40c046caf2bc4e0edb6e4742e
Arpit Gupta <gupta98960@gmail.com>
2025-11-01 11:55:13 +0530

    update product page mobile view

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -614,7 +614,7 @@ const Products = () => {
       </section>
 
       {/* Features Banner */}
-      <section className="py-16 bg-secondary">
+      {/* <section className="py-16 bg-secondary">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
             <div className="space-y-2">
@@ -639,7 +639,7 @@ const Products = () => {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
     </div>
   );
 };
